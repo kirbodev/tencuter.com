c32e0e4dd79be14c4a7533ba35df79b388f3ce44
kirbodev <superkirbybiz@email.com>
2022-12-20 21:48:34 +0000

    added animated support to deepfry

--- functions/deepfry.js
+++ functions/deepfry.js
@@ -1,20 +1,27 @@
 const fetch = require("isomorphic-fetch");
-const jimp = require("jimp");
+const cgif = require("canvas-gif");
+const gifInfo = require("gif-info");
 
 module.exports.handler = async (event, context, callback) => {
   try {
-    // Deepfry the image
-    const gif = event.path.split("/").pop();
-    const gifurl = await fetch(`https://tenor.com/view/${gif}`);
-    const resultText = await gifurl.text();
-    const resultGif = resultText.match(/https:\/\/media.tenor.com\/[a-z0-9]+/i)[0];
-    const result = await fetch(resultGif);
-    const buffer = await result.buffer();
-    const image = await jimp.read(buffer);
-    // Pixelate slightly, set contrast to 0.95 and posterize to 8. Then, sharpen the image. Then, make it very low quality. Then, make it very high quality.
-    image.pixelate(2).contrast(0.95).posterize(8).quality(1).quality(100);
-    const data = await image.getBufferAsync(jimp.MIME_JPEG);
+  // Deepfry the image
+  const gif = event.path.split("/").pop();
+  const gifurl = await fetch(`https://tenor.com/view/${gif}`);
+  const resultText = await gifurl.text();
+  const resultGif = resultText.match(
+    /https:\/\/media.tenor.com\/[a-z0-9]+/i
+  )[0];
+  const result = await fetch(resultGif);
+  const buffer = await result.buffer();
+  const arrayBuffer = new Uint8Array(buffer).buffer;
+  const info = gifInfo(arrayBuffer);
+  const fps = info.images.length / (info.duration / 1000);
 
+  cgif(buffer, deepfry, {
+    optimiser: true,
+    fps: fps,
+    quality: 50,
+  }).then((data) => {
     callback(null, {
       statusCode: 200,
       headers: {
@@ -23,6 +30,7 @@ module.exports.handler = async (event, context, callback) => {
       body: data.toString("base64"),
       isBase64Encoded: true,
     });
+  });
   } catch (e) {
     callback(null, {
       statusCode: 500,
@@ -30,3 +38,54 @@ module.exports.handler = async (event, context, callback) => {
     });
   }
 };
+
+function deepfry(context, width, height, totalFrames, currentFrame) {
+  saturate(context, 0, 0, width, height);
+  contrast(context, 0, 0, width, height);
+  posterise(context, 0, 0, width, height);
+}
+
+function saturate(context, x, y, width, height, amount = -15) {
+  const imageData = context.getImageData(x, y, width, height);
+  const data = imageData.data;
+
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      const dest = (i * width + j) * 4;
+      const grey = Number.parseInt(
+        0.2125 * data[dest] + 0.7154 * data[dest + 1] + 0.0721 * data[dest + 2]
+      );
+      data[dest] += amount * (grey - data[dest]);
+      data[dest + 1] += amount * (grey - data[dest + 1]);
+      data[dest + 2] += amount * (grey - data[dest + 2]);
+    }
+  }
+
+  context.putImageData(imageData, x, y);
+}
+
+function contrast(context, x, y, width, height, amount = 10) {
+  const imageData = context.getImageData(x, y, width, height);
+  const data = imageData.data;
+
+  for (let i = 0; i < data.length; i += 4) {
+    data[i] += (amount * (data[i] - 128)) / 100;
+    data[i + 1] += (amount * (data[i + 1] - 128)) / 100;
+    data[i + 2] += (amount * (data[i + 2] - 128)) / 100;
+  }
+
+  context.putImageData(imageData, x, y);
+}
+
+function posterise(context, x, y, width, height, amount = 30) {
+  const imageData = context.getImageData(x, y, width, height);
+  const data = imageData.data;
+
+  for (let i = 0; i < data.length; i += 4) {
+    data[i] = Math.floor(data[i] / amount) * amount;
+    data[i + 1] = Math.floor(data[i + 1] / amount) * amount;
+    data[i + 2] = Math.floor(data[i + 2] / amount) * amount;
+  }
+
+  context.putImageData(imageData, x, y);
+}
